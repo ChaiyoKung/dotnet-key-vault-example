e7ff9506539d2cc586a6093d90ab57877b6ce598
ChaiyoKung <kananek45@gmail.com>
2024-10-04 15:21:40 +0700

    feat: get azure key vault secret

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+
+SecretClientOptions options = new SecretClientOptions()
+{
+  Retry =
+  {
+    Delay = TimeSpan.FromSeconds(2),
+    MaxDelay = TimeSpan.FromSeconds(16),
+    MaxRetries = 5,
+    Mode = RetryMode.Exponential
+  }
+};
+var client = new SecretClient(new Uri("https://<your-unique-key-vault-name>.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+KeyVaultSecret secret = client.GetSecret("<mySecret>");
+Console.WriteLine(secret.Value);
