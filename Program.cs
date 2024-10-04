6401313620a1d514c857a5f834866214e57ceae7
ChaiyoKung <kananek45@gmail.com>
2024-10-04 15:39:46 +0700

    refactor: get key vault uri from `appsettings.json`

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 ﻿using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Core;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+  .SetBasePath(Directory.GetCurrentDirectory())
+  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+  .Build();
 
 SecretClientOptions options = new SecretClientOptions()
 {
@@ -12,7 +18,8 @@ SecretClientOptions options = new SecretClientOptions()
     Mode = RetryMode.Exponential
   }
 };
-var client = new SecretClient(new Uri("https://<your-unique-key-vault-name>.vault.azure.net/"), new DefaultAzureCredential(), options);
+var keyVaultUri = config["KeyVault:Uri"] ?? throw new ArgumentNullException("KeyVault:Uri configuration is missing");
+var client = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential(), options);
 
-KeyVaultSecret secret = client.GetSecret("<mySecret>");
+KeyVaultSecret secret = client.GetSecret("my-secret");
 Console.WriteLine(secret.Value);
